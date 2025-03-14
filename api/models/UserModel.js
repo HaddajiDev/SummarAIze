622a5ecc97348d696ba27b7966c95ffeab02487c
MaizaAymen <aymenmaiza87@gmail.com>
2025-03-14 18:00:45 +0100

    add isAdmin field and clean up comments in UserModel schema

--- api/models/UserModel.js
+++ api/models/UserModel.js
@@ -4,12 +4,14 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false },
+//-OTP for account verification-Expiration timestamp for verifyotp---Indicates if the account is verified-- OTP for password reset--Expiration timestamp for resetOtp---------//
     verifyotp: { type: String, default: '' },
     verifyotpexpireAt: { type: Number, default: 0 },
     isAccountVerified: { type: Boolean, default: false },
     resetOtp: { type: String, default: '' },
-    resetOtpExpireAt: { type: Number, default: 0 },
-    isAdmin: { type: Boolean, default: false }
+    resetOtpExpireAt: { type: Number, default: 0 }
+    
 });
 
 const userModel = mongoose.models.user || mongoose.model('user', userSchema);
