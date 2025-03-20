3a088ef3a2f45157cc63be8b6e43ad1eb8adbb34
HaddajiDev <ahmedhaddajiahmed@gmail.com>
2025-03-20 17:38:32 +0100

    cors

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5173", "https://summar-ai-ze.vercel.app/"],
   credentials: true
 }));
 app.use(express.json());
